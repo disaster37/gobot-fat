d69683552956f3f11be29530c8a90dcd262cf9dc
LANGOUREAUX Sebastien <Sebastien.LANGOUREAUX@harmonie-mutuelle.fr>
2021-01-12 11:34:16 +0100

    Add some trace

--- dfp/board/board.go
+++ dfp/board/board.go
@@ -3,6 +3,7 @@ package dfpboard
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/disaster37/gobot-fat/dfp"
@@ -11,6 +12,7 @@ import (
 	"github.com/disaster37/gobot-fat/usecase"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"github.com/stianeikeland/go-rpio/v4"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
@@ -166,25 +168,25 @@ func (h *DFPBoard) Start(ctx context.Context) (err error) {
 		h.captorWaterUnder,
 		h.captorWaterUpper,
 	}
-	/*
-		if err := rpio.Open(); err != nil {
-			log.Errorf("Error when open rpio: %s", err.Error())
+
+	if err := rpio.Open(); err != nil {
+		log.Errorf("Error when open rpio: %s", err.Error())
+		return err
+	}
+
+	for _, button := range listPins {
+
+		pinNumber, err := strconv.ParseInt(button.Pin(), 10, 32)
+		if err != nil {
 			return err
 		}
-	*/
-	for _, button := range listPins {
-		/*
-			pinNumber, err := strconv.ParseInt(button.Pin(), 10, 32)
-			if err != nil {
-				return err
-			}
-			pin := rpio.Pin(pinNumber)
-			pin.Input()
-			pin.PullUp()
-		*/
+		pin := rpio.Pin(pinNumber)
+		pin.Input()
+		pin.PullUp()
+
 		button.DefaultState = 1
 	}
-	//rpio.Close()
+	rpio.Close()
 
 	log.Infof("RPIO initialized")
 	h.captorSecurityUpper.DefaultState = 0
