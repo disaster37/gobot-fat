abe4619259527f1544095066cb4983ffb1640e76
LANGOUREAUX Sebastien <Sebastien.LANGOUREAUX@harmonie-mutuelle.fr>
2021-03-02 12:45:38 +0100

    Send event on ES on routine to avoid blocking current action

--- helper/event.go
+++ helper/event.go
@@ -27,27 +27,31 @@ const (
 )
 
 // SendEvent permit to send event on Elasticsearch
+// Run it on routine to avoid block action
 func SendEvent(ctx context.Context, esUsecase usecase.UsecaseCRUD, sourceName string, kind string, name string, args ...interface{}) {
 
-	event := &models.Event{
-		SourceName: sourceName,
-		Timestamp:  time.Now(),
-		EventType:  name,
-		EventKind:  kind,
-	}
-
-	// Add extra infos
-	if len(args) > 0 {
-		switch kind {
-		case KindEventTemperature:
-			event.Temperature = args[0].(float64)
-			break
-		case KindEventTankLevel:
-			event.Level = args[0].(int64)
+	go func() {
+		event := &models.Event{
+			SourceName: sourceName,
+			Timestamp:  time.Now(),
+			EventType:  name,
+			EventKind:  kind,
 		}
-	}
 
-	if err := esUsecase.Create(ctx, event); err != nil {
-		log.Errorf("Error when store new event: %s", err.Error())
-	}
+		// Add extra infos
+		if len(args) > 0 {
+			switch kind {
+			case KindEventTemperature:
+				event.Temperature = args[0].(float64)
+				break
+			case KindEventTankLevel:
+				event.Level = args[0].(int64)
+			}
+		}
+
+		if err := esUsecase.Create(ctx, event); err != nil {
+			log.Errorf("Error when store new event: %s", err.Error())
+		}
+	}()
+
 }
