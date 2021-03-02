3452bbfab7e30a800da84435734c6a757261aeb1
LANGOUREAUX Sebastien <Sebastien.LANGOUREAUX@harmonie-mutuelle.fr>
2021-03-02 10:48:18 +0100

    Fix mailer

--- mock/mock_mail.go
+++ mock/mock_mail.go
@@ -4,7 +4,7 @@ import "github.com/disaster37/gobot-fat/mail"
 
 type MockMail struct{}
 
-func (m *MockMail) SendEmail(title string, contend string) (err error) { return }
+func (m *MockMail) SendEmail(title string, contend string) { return }
 
 func NewMockMail() mail.Mail {
 	return &MockMail{}
