dc074434f1fa0c905f4e52ee10d6788cb9ab24ad
disaster37 <linuxworkgroup@hotmail.com>
2025-08-18 12:49:27 +0000

    fix: fix delivery for state
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- tfpstate/delivery/http/tfp_state_handler.go
+++ tfpstate/delivery/http/tfp_state_handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/disaster37/gobot-fat/models"
 	"github.com/disaster37/gobot-fat/tfpstate"
@@ -24,6 +25,7 @@ func NewTFPStateHandler(e *echo.Group, us usecase.UsecaseCRUD) {
 		us: us,
 	}
 	e.GET("/tfp-states", handler.Get)
+	e.PATCH("/tfp-states/:id", handler.Update)
 	e.POST("/tfp-states", handler.UpdateOld)
 
 }
@@ -53,7 +55,8 @@ func (h *TFPStateHandler) Get(c echo.Context) error {
 	return jsonapi.MarshalOnePayloadEmbedded(c.Response(), state)
 }
 
-// UpdateOld permit to update the current TFP state
+// Update permit to update the current TFP state
+// We can only update field about nbHourUVC / nbHourOzone
 func (h *TFPStateHandler) UpdateOld(c echo.Context) error {
 	var err error
 	ctx := c.Request().Context()
@@ -62,8 +65,9 @@ func (h *TFPStateHandler) UpdateOld(c echo.Context) error {
 	}
 	c.Response().Header().Set(echo.HeaderContentType, jsonapi.MediaType)
 
-	state := &models.TFPState{}
-	if err = jsonapi.UnmarshalPayload(c.Request().Body, state); err != nil {
+	// Get the expected TFPState
+	expectedState := &models.TFPState{}
+	if err = jsonapi.UnmarshalPayload(c.Request().Body, expectedState); err != nil {
 		c.Response().WriteHeader(http.StatusBadRequest)
 		return jsonapi.MarshalErrors(c.Response(), []*jsonapi.ErrorObject{
 			{
@@ -73,11 +77,23 @@ func (h *TFPStateHandler) UpdateOld(c echo.Context) error {
 			},
 		})
 	}
-	state.ID = tfpstate.ID
+	expectedState.ID = tfpstate.ID
+	log.Debugf("Expected TFPState: %+v", expectedState)
 
-	log.Debugf("Data: %+v", state)
+	// Get the current TFPState
+	currentState := &models.TFPState{}
+	if err := h.us.Get(ctx, tfpstate.ID, currentState); err != nil {
+		return err
+	}
+	log.Debugf("Current TFPState: %+v", expectedState)
 
-	if err = h.us.Update(ctx, state); err != nil {
+	// Compute the final state
+	currentState.OzoneBlisterNbHour = expectedState.OzoneBlisterNbHour
+	currentState.UVC1BlisterNbHour = expectedState.UVC1BlisterNbHour
+	currentState.UVC2BlisterNbHour = expectedState.UVC2BlisterNbHour
+	log.Debugf("Final TFPState: %+v", currentState)
+
+	if err = h.us.Update(ctx, currentState); err != nil {
 		log.Errorf("Error when update tfp_state: %s", err.Error())
 		c.Response().WriteHeader(http.StatusInternalServerError)
 		return jsonapi.MarshalErrors(c.Response(), []*jsonapi.ErrorObject{
@@ -90,5 +106,69 @@ func (h *TFPStateHandler) UpdateOld(c echo.Context) error {
 	}
 
 	c.Response().WriteHeader(http.StatusCreated)
-	return jsonapi.MarshalOnePayloadEmbedded(c.Response(), state)
+	return jsonapi.MarshalOnePayloadEmbedded(c.Response(), currentState)
+}
+
+// Update permit to update the current TFP config
+func (h *TFPStateHandler) Update(c echo.Context) error {
+	var err error
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	c.Response().Header().Set(echo.HeaderContentType, jsonapi.MediaType)
+
+	// Get the expected TFPState
+	expectedState := &models.TFPState{}
+	if err = jsonapi.UnmarshalPayload(c.Request().Body, expectedState); err != nil {
+		c.Response().WriteHeader(http.StatusBadRequest)
+		return jsonapi.MarshalErrors(c.Response(), []*jsonapi.ErrorObject{
+			{
+				Status: fmt.Sprintf("%d", http.StatusBadRequest),
+				Title:  "Error when update tfp_state",
+				Detail: err.Error(),
+			},
+		})
+	}
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.Response().WriteHeader(http.StatusBadRequest)
+		return jsonapi.MarshalErrors(c.Response(), []*jsonapi.ErrorObject{
+			{
+				Status: fmt.Sprintf("%d", http.StatusBadRequest),
+				Title:  "Error when update tfp_state",
+				Detail: err.Error(),
+			},
+		})
+	}
+	expectedState.ID = uint(id)
+	log.Debugf("Expected TFPState: %+v", expectedState)
+
+	// Get the current TFPState
+	currentState := &models.TFPState{}
+	if err := h.us.Get(ctx, uint(id), currentState); err != nil {
+		return err
+	}
+	log.Debugf("Current TFPState: %+v", expectedState)
+
+	// Compute the final state
+	currentState.OzoneBlisterNbHour = expectedState.OzoneBlisterNbHour
+	currentState.UVC1BlisterNbHour = expectedState.UVC1BlisterNbHour
+	currentState.UVC2BlisterNbHour = expectedState.UVC2BlisterNbHour
+	log.Debugf("Final TFPState: %+v", currentState)
+
+	if err = h.us.Update(ctx, currentState); err != nil {
+		log.Errorf("Error when update tfp_state: %s", err.Error())
+		c.Response().WriteHeader(http.StatusInternalServerError)
+		return jsonapi.MarshalErrors(c.Response(), []*jsonapi.ErrorObject{
+			{
+				Status: fmt.Sprintf("%d", http.StatusInternalServerError),
+				Title:  "Error when update tfp_state",
+				Detail: err.Error(),
+			},
+		})
+	}
+
+	c.Response().WriteHeader(http.StatusCreated)
+	return jsonapi.MarshalOnePayloadEmbedded(c.Response(), currentState)
 }
